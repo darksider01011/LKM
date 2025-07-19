1e8b967c01b845734e53eeb0c7603955fdbcaeb9
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2025-07-19 14:24:58 +0330

    Update hello.c

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,19 +1,30 @@
+// Kernel initialization functions
 #include <linux/init.h>
+
+// Kernel function 
 #include <linux/kernel.h>
+
+// Add functionality to the kernel
 #include <linux/module.h>
 
+// Module license type
 MODULE_LICENSE("GPL");
 
-
+// Initialization function 
+// The "__init" keyword tells the kernel to discard this function after it is called, to save memory
 static int __init module_start(void){
+    // Print hello, world to kernel buffer
     printk(KERN_INFO "Hello, World!\n");
     return 0;
 }
 
-
+// Cleanup function
+// The "__init" keyword tells the kernel to discard this function after it is called, to save memory
 static void __exit module_end(void){
     printk(KERN_INFO "Goodbye, World!\n");
 }
 
+// Registers the initialization function with the kernel
 module_init(module_start);
+// Registers the cleanup function with the kernel 
 module_exit(module_end);
